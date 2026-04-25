569864cc6975ed4377738d9a5882608219f90fec
vikrammahto <vikramkr.in@gmail.com>
2026-04-25 18:30:13 +0530

    refactor: standardize quotes in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,18 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: "https",
-        hostname: "i.pravatar.cc",
-        port: "",
-        pathname: "/100",
-        search: "?img=5",
+        protocol: 'https',
+        hostname: 'i.pravatar.cc',
+        port: '',
+        pathname: '/100',
+        search: '?img=5',
       },
     ],
   },
+  output: 'standalone',
 };
 
 export default nextConfig;
